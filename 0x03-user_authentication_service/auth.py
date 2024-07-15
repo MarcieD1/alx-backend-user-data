23dd462cc634475065f37e84d607222f5c7922b0
MarcieD1 <marciatarisai@icloud.com>
2024-07-15 12:13:39 +0200

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+# auth.py
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
