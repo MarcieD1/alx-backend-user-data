1a248d4311932cb8813b04a27a35501dbace3009
MarcieD1 <marciatarisai@icloud.com>
2024-07-17 15:39:04 +0200

    Update Declarative_base function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
-"""DB module
+""" DB module
 """
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, Session, declarative_base
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
 from typing import TypeVar
 
-Base = declarative_base()
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
-
-class CustomInvalidRequestError(Exception):
-    pass
 
 class DB:
-    """DB class
+    """
+    DB class.
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Constructor.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+        _session.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -42,54 +40,36 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Adds a new user to the Database.
+        add_user.
         """
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
         """
-        Finds a User in the Database.
+        find_user_by.
         """
-        for key in kwargs.keys():
-            if not hasattr(User, key):
-                raise CustomInvalidRequestError()
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-
-        if user:
-            return user
-        raise NoResultFound()
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
-        updating a user in the database
+        update_user.
         """
-        VALID_FIELDS = {'email', 'hashed_password', 'session_id', 'reset_token'}
+        session = self._session
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
             if k not in VALID_FIELDS:
-                raise ValueError(f"Invalid field: {k}")
+                raise ValueError
             setattr(user, k, v)
-        self._session.commit()
-
-if __name__ == '__main__':
-    my_db = DB()
-
-    user = my_db.add_user("test@test.com", "PwdHashed")
-    print(f"Added user with ID: {user.id}")
-
-    try:
-        find_user = my_db.find_user_by(email="test2@test.com")
-        print(f"Found user with ID: {find_user.id}")
-    except NoResultFound:
-        print("User not found")
-
-    try:
-        find_user = my_db.find_user_by(no_email="test@test.com")
-        print(f"Found user with ID: {find_user.id}")
-    except CustomInvalidRequestError:
-        print("Invalid request")
-
+        session.commit()
