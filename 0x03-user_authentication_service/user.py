5c60ac72954a39454b55e671239e7a62effad0bb
MarcieD1 <marciatarisai@icloud.com>
2024-07-17 14:50:32 +0200

    User Update again

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
-"""_summary_
+"""
+Module to create user database
 """
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
 Base = declarative_base()
 
 
 class User(Base):
-    """_summary_
-
-    Args:
-        Base (_type_): _description_
+    """
+    Creates table for user
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+
+if __name__== '__main__':
+    print(User.__tablename__)
+
+    for column in User.__table__.columns:
+        print("{}: {}.".format(column, column.type))
